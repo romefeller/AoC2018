0cc17ff3444417f5a8080d37753d767915b97c5a
romefeller <algarcia@usp.br>
2018-12-04 20:41:37 +0000

    regex complete

--- Day4/Main.hs
+++ Day4/Main.hs
@@ -21,8 +21,16 @@ data GuardAction = GuardAction {
     action :: Action
 } deriving Show
 
+toGuardAction :: Maybe [String] -> GuardAction
+toGuardAction Nothing = GuardAction 0 0 0 0 0 0 Sleep
+toGuardAction (Just (y:m:d:h:n:a:"":[])) = 
+    GuardAction (read y) (read m) (read d) (read h) (read n) 0 (readAction a) 
+toGuardAction (Just (y:m:d:h:n:a:i:[])) = 
+    GuardAction (read y) (read m) (read d) (read h) (read n) (read i) (readAction a)  
+toGuardAction (Just _) = GuardAction 0 0 0 0 0 0 Shift
+
 main :: IO ()
 main = do 
     let r = mkRegex "\\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\\] (falls asleep|wakes up|Guard #([0-9]+) begins shift)"
-    registers <- readFile "input"
-    putStrLn $ show $ map (matchRegex r . T.unpack) $ T.splitOn "\n" $ T.pack registers
+    registers <- readFile "test"
+    putStrLn $ show $ map (toGuardAction . matchRegex r . T.unpack) $ T.splitOn "\n" $ T.pack registers
