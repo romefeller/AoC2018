1f90eadbedde5b3f567997034a1e4111c2cbe777
romefeller <algarcia@usp.br>
2018-12-05 05:57:22 +0000

    Day 5 part 2

--- Day5/Main.hs
+++ Day5/Main.hs
@@ -1,11 +1,20 @@
 module Main where 
 
 import Data.Char
+import Control.Applicative
 
 reduce :: String -> String
 reduce = foldl (\e c -> if abs(ord c - ord (head e)) == 32 then (tail e) else c:e) "@"
 
+--- PART 2 ---
+filterChar :: Char -> String -> String
+filterChar x = filter (/= x) . filter (/= (chr((ord x) + 32)))
+
+exec :: Char -> String -> String
+exec c s = (tail . reverse) $ reduce $ filterChar c s
+-------------
 main :: IO ()
 main = do 
     polymer <- readFile "input"
     print $ length $ (tail . reverse) $ reduce polymer
+    print $ minimum . map length $ (exec <$> ['A'..'Z']) <*> [polymer]
